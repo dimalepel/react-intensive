0642938119cbe8db4a934d4435ed1797bf373e88
Dimitry Vashkevich <dimalepel@mail.ru>
2025-12-21 19:58:58 +0300

    fix: удалить использование useMemo

--- src/widgets/PostList/PostList.tsx
+++ src/widgets/PostList/PostList.tsx
@@ -1,27 +1,23 @@
 import PostCard from "../../entities/post/ui/PostCard.tsx"
 import styles from './PostList.module.css'
 import type { Post } from '../../entities/post/model/Post.ts';
-import {memo, useMemo} from "react";
+import {memo} from "react";
 
 interface PostListProps {
     posts: Post[];
 }
 
 const PostList =  memo(function PostList({ posts }: PostListProps) {
-    const postCards = useMemo(() => {
-        return posts.map(post => (
-            <PostCard
-                title={post.title}
-                content={post.body}
-                comments={post.comments}
-                key={post.id}
-            />
-        ));
-    }, [posts]);
-
     return (
         <ul className={styles['post-list']}>
-            { postCards }
+            { posts.map(post => (
+                <PostCard
+                    title={post.title}
+                    content={post.body}
+                    comments={post.comments}
+                    key={post.id}
+                />
+            )) }
         </ul>
     )
 })
